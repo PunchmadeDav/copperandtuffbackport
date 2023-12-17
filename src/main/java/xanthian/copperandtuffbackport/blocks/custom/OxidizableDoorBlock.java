5901592c80e07aa226a15f9888ba40aa95387230
Xanthian <xanneh@hotmail.com>
2023-12-17 12:48:04 +0000

    1.20.1 Support for 1.20.2 Fixes broken doors when waxed or scraped

--- src/main/java/xanthian/copperandtuffbackport/blocks/custom/OxidizableDoorBlock.java
+++ src/main/java/xanthian/copperandtuffbackport/blocks/custom/OxidizableDoorBlock.java
@@ -1,13 +1,12 @@
 package xanthian.copperandtuffbackport.blocks.custom;
 
-import net.minecraft.block.BlockSetType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.DoorBlock;
-import net.minecraft.block.Oxidizable;
+import net.minecraft.block.*;
 import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.WorldAccess;
 
 public class OxidizableDoorBlock extends DoorBlock implements Oxidizable {
     private final OxidationLevel oxidationLevel;
@@ -26,6 +25,16 @@ public class OxidizableDoorBlock extends DoorBlock implements Oxidizable {
 
     public boolean hasRandomTicks(BlockState state) {
         return Oxidizable.getIncreasedOxidationBlock(state.getBlock()).isPresent();
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        DoubleBlockHalf doubleBlockHalf = state.get(HALF);
+        if (direction.getAxis() == Direction.Axis.Y && doubleBlockHalf == DoubleBlockHalf.LOWER == (direction == Direction.UP)) {
+            return neighborState.getBlock() instanceof DoorBlock && neighborState.get(HALF) != doubleBlockHalf ? neighborState.with(HALF, doubleBlockHalf) : Blocks.AIR.getDefaultState();
+        } else {
+            return doubleBlockHalf == DoubleBlockHalf.LOWER && direction == Direction.DOWN && !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+        }
     }
 
     public OxidationLevel getDegradationLevel() {
